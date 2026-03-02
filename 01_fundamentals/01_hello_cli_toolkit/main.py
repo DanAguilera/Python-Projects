8c43133a269346fcd174e5a4b3592e84f0e16551
DanAguilera <dan.aguilera97@gmail.com>
2026-03-02 17:16:19 -0500

    Initial commit, and basic menu loop created for fundamentals folder

---
+++ 01_fundamentals/01_hello_cli_toolkit/main.py
@@ -0,0 +1,31 @@
+def menu_loop():
+    while True:
+        print("Welcome to the CLI Toolkit!")
+        print("1. Tip Calculator")
+        print("2. BMI Calculator")
+        print("3. Simple Interest Calculator")
+        print("4. Countdown Timer")
+        print("5. Password Strength Checker")
+        print("0. Exit")
+
+        choice = input("Please select an option: ")
+
+        if choice == '1':
+            print("You selected Option 1")
+        elif choice == '2':
+            print("You selected Option 2")
+        elif choice == '3':
+            print("You selected Option 3")
+        elif choice == '4':
+            print("You selected Option 4")
+        elif choice == '5':
+            print("You selected Option 5")
+        elif choice == '0':
+            print("Exiting the CLI Toolkit. Goodbye!")
+            break
+        else:
+            print("Invalid option, please try again.")
+
+if __name__ == "__main__":
+    menu_loop()
+
